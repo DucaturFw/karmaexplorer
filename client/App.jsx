10110cdf4830ee0aa94a20a0008d1ea2bdb598fd
mikeshukshin <merqrylive@gmail.com>
2018-07-03 18:48:22 +0300

    Remove obsolete components

--- client/App.jsx
+++ client/App.jsx
@@ -5,14 +5,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import theme from './styles/theme';
 import './styles/styles.css';
+
 import Container from './components/container/index'
 import Header from './components/header';
 import Footer from './components/footer';
 import MainContent from './components/maincontent/index';
 import Distribution from './components/distribution/index';
 import Exchange from './components/exchange/index';
-import Choose from './components/choose/index';
-import Rate from './components/rate/index';
 import Network from './components/network/index';
 import Pending from './components/pending/index';
 
@@ -29,9 +28,6 @@ export default class App extends Component {
                   <Route exact path="/" component={Distribution} />
                   <Route exact path="/holders/:network" component={Network} />
                   <Route exact path="/exchange" component={Exchange} />
-                  <Route exact path="/choose" component={Choose} />
-                  <Route exact path="/rate" component={Rate} />
-                  {/* <Route exact path="/network" component={Network} /> */}
                   <Route exact path="/pending" component={Pending} />
                 </MainContent >
                 <Footer />
